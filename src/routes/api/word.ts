da204daad45ad7b4244e395402fbb0c3e6b3ae97
RahulNavneeth <rahulmnavneeth@gmail.com>
2022-06-18 00:13:08 +0530

    Date bug fix #1

--- src/routes/api/word.ts
+++ src/routes/api/word.ts
@@ -7,26 +7,26 @@ export const get = () => {
 	const words: word[] = [
 		{
 			word: 'CONST',
-			date: '06/16/2022',
+			date: '6/16/2022',
 			info: 'A keyword that defines a variable or pointer as unchangeable.'
 		},
 		{
 			word: 'FLOOR',
-			date: '06/17/2022',
+			date: '6/17/2022',
 			info: 'Function returns the largest integer less than or equal to a given number.'
 		},
 		{
 			word: 'ASYNC',
-			date: '06/18/2022',
+			date: '6/18/2022',
 			info: 'Ensures that the function returns a promise, and wraps non-promises in it.'
 		},
 		{
 			word: 'AWAIT',
-			date: '06/16/2022',
+			date: '6/16/2022',
 			info: 'Inside an async function you can use the await keyword before a call to a function that returns a promise.'
 		}
 	];
-	const word = words.find((element) => element.date === parseDate(new Date()));
+	const word = words.find((element) => element.date === new Date().toLocaleDateString());
 	if (word) {
 		const { date, ...data } = word;
 		return {
