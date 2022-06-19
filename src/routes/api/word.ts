c2a01c9178669ceb03ad084b2d588937f7fb37e1
RahulNavneeth <rahulmnavneeth@gmail.com>
2022-06-19 23:35:00 +0530

    Adding standin words

--- src/routes/api/word.ts
+++ src/routes/api/word.ts
@@ -24,6 +24,26 @@ export const get = () => {
 			word: 'AWAIT',
 			date: '06/19/2022',
 			info: 'Inside an async function you can use the await keyword before a call to a function that returns a promise.'
+		},
+		{
+			word: 'BREAK',
+			date: '06/20/2022',
+			info: 'Loop control statement which is used to terminate the loop.'
+		},
+		{
+			word: 'FLOOR',
+			date: '06/21/2022',
+			info: 'A variable type that is used to store floating-point number values.'
+		},
+		{
+			word: 'WHILE',
+			date: '06/22/2022',
+			info: 'A simple programming language constructed from assignments, sequential composition, conditionals and while statements.'
+		},
+		{
+			word: 'CLASS',
+			date: '06/23/2022',
+			info: 'An extensible program-code-template for creating objects, providing initial values for state and implementations of behavior.'
 		}
 	];
 	const word = words.find((element) => element.date === parseDate(new Date()));
